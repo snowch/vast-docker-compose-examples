b314a7e553c465b52fd7fd66afd1e1a93b0bf5d5
Chris Snow <chsnow123@gmail.com>
2024-11-12 11:22:13 +0000

    updated

--- jupyter-pyspark/endpoints.sh
+++ jupyter-pyspark/endpoints.sh
@@ -8,6 +8,5 @@ source ../.env-local
 TOKEN=$(docker compose logs | grep 'jupyter-pyspark-1 *| *http://127.0.0.1:8888/lab?token=' | awk -F'token=' '{print $2}')
 
 echo "Jupyter-Spark:"
-echo "http://${DOCKER_HOST_OR_IP}:8888"
-echo "Token: ${TOKEN}"
+echo "http://${DOCKER_HOST_OR_IP}:8888/lab?token=${TOKEN}"
 echo ""
