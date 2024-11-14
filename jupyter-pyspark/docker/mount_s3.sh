65db06ca1adcf0b7c56c231a755aeed58ed2f23b
Chris Snow <chsnow123@gmail.com>
2024-11-14 12:35:33 +0000

    added

--- jupyter-pyspark/docker/mount_s3.sh
+++ jupyter-pyspark/docker/mount_s3.sh
@@ -6,14 +6,17 @@ export AWS_ACCESS_KEY_ID=${S3A_ACCESS_KEY}
 export AWS_SECRET_ACCESS_KEY=${S3A_SECRET_KEY}
 export AWS_ENDPOINT_URL=${S3A_ENDPOINT}
 
-mkdir -p ${HOME}/s3
+mkdir -p ${HOME}/s3-read-only
+
+# read-only until more info on
+# https://github.com/awslabs/mountpoint-s3/issues/1133
 
 mount-s3 \
     --region VAST \
     --endpoint-url $AWS_ENDPOINT_URL \
-    --allow-delete \
+    --read-only \
     --uid $(id -u jovyan) \
     --gid $(id -g jovyan) \
     --file-mode 0664 \
     --dir-mode 0775 \
-    "$S3A_BUCKET" ${HOME}/s3
+    "$S3A_BUCKET" ${HOME}/s3-read-only
