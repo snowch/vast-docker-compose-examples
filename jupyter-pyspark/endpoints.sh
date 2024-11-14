efb5d6fe40494a2251cde193893c894a0ed93fe7
Chris Snow <chsnow123@gmail.com>
2024-11-14 08:36:11 +0000

    support compose with hyphen

--- jupyter-pyspark/endpoints.sh
+++ jupyter-pyspark/endpoints.sh
@@ -5,7 +5,13 @@ cd "$(dirname "$0")"
 
 source ../.env-local
 
-TOKEN=$(docker compose logs | grep 'jupyter-pyspark-1 *| *http://127.0.0.1:8888/lab?token=' | awk -F'token=' '{print $2}')
+# Check if 'docker compose' is available; if not, use 'docker-compose'
+DOCKER_COMPOSE_CMD="docker compose"
+if ! command -v docker compose &> /dev/null; then
+  DOCKER_COMPOSE_CMD="docker-compose"
+fi
+
+TOKEN=$($DOCKER_COMPOSE_CMD logs | grep 'jupyter-pyspark-1 *| *http://127.0.0.1:8888/lab?token=' | awk -F'token=' '{print $2}')
 
 echo "Jupyter-Spark:"
 echo "http://${DOCKER_HOST_OR_IP}:8888/lab?token=${TOKEN}"
