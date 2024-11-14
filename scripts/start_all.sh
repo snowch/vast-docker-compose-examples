c6fc5401d77a7128f4f2397259ae76169e83ecfc
Chris Snow <chsnow123@gmail.com>
2024-11-14 08:50:49 +0000

    updated

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -10,5 +10,16 @@ for dir in $(ls -d */ | grep -v "^demos/" | grep -v "^scripts/"); do
   (cd $dir && $DOCKER_COMPOSE_CMD up -d && $DOCKER_COMPOSE_CMD up --wait)
 done
 
+echo "======================================"
+echo "        Starting Trino Setup          "
+echo "======================================"
 ./trino/setup_iceberg.sh
+
+echo "======================================"
+echo "       Starting Superset Setup        "
+echo "======================================"
 ./superset/setup_db_connections.sh
+
+echo "======================================"
+echo "       All Setup Tasks Complete       "
+echo "======================================"
