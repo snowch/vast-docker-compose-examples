a4f4bc0ab141cadc9ea3b1fa66ec21bfd785c251
Chris Snow <chsnow123@gmail.com>
2024-11-29 13:36:39 +0000

    add trino ui

--- trino/endpoints.sh
+++ trino/endpoints.sh
@@ -9,3 +9,11 @@ source .env
 echo "Trino Server:"
 echo "https://${DOCKER_HOST_OR_IP}:8443"
 echo ""
+
+echo "Trino Server UI:"
+echo "https://${DOCKER_HOST_OR_IP}:8443/ui"
+echo ""
+
+echo "Note: 404 error with trino queries happens when"
+echo "      trino is unavailable, e.g. if it has crashed"
+echo "      and is restarting"
